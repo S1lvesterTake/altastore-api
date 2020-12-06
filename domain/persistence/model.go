c5d42d329a504fa52613bfd0fb028e382e72f70b
silvester <silvetake@gmail.com>
2020-12-06 20:13:07 +0700

    DB base model

---
+++ domain/persistence/model.go
@@ -0,0 +1,17 @@
+package persistence
+
+import "time"
+
+//Model DB base model
+type Model struct {
+	ID        uint64    `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+//ModelSoftDelete DB base model for delete
+type ModelSoftDelete struct {
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at"`
+}
